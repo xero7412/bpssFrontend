5d49d274874faa5835667b25ca21c029480bd546
Toshi <toshosharma1208@gmail.com>
2019-05-13 09:36:18 +0530

    BPSS 1.0.0

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  //To retrive data from the backend server.
+
   title = 'bpssFrontend';
+  baseUrl = 'https://my-json-server.typicode.com/xero7412/bpss_backend'; //Server URL
+  arrInventory = []; //array to store the inventory list
+  constructor(private httpClient: HttpClient){}
+  ngOnInit () {
+    this.httpClient.get(this.baseUrl + '/inventory').subscribe((res : any[])=>{
+        this.arrInventory = res;
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
